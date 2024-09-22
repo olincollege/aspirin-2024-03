9c7e0a19a55e974e4aa4a6a1d53233909a24fcb6
Daniel Sudzilouski <daniel@sudzilouski.com>
2024-09-22 12:05:27 -0400

    start basics

--- assignments/01-ownership-borrowing/src/basics.rs
+++ assignments/01-ownership-borrowing/src/basics.rs
@@ -1,30 +1,39 @@
 /// Take ownership of the passed in string and print it
-fn takes_ownership_and_prints() {
-    todo!();
+fn takes_ownership_and_prints(s: String) {
+    println!("{s}");
 }
 
 /// Take a string slice and return the first letter, or None in the case
 /// of a blank string
-fn first_letter() {
-    todo!();
+fn first_letter(s: &str) -> Option<&str> {
+    match s.chars().next() {
+        Some(c) => Some(&s[..c.len_utf8()]),
+        None => None
+    }
 }
 
 type Student = (String, u32);
 
 /// Given a reference to a student, return the student's name
-fn get_name() {
-    todo!();
+fn get_name(student: &Student) -> String {
+    student.0.clone()
 }
 
 /// Given a slice of i32s, return the sum of the elements
-fn slice_sum() {
-    todo!();
+fn slice_sum(arr: &[i32]) -> i32 {
+    arr.iter().sum()
 }
 
 /// Given a string slice, look for a substring, and return a slice of the first
 /// occurrence of the substring (return None if the substring is not found)
-fn find_in_string() {
-    todo!();
+/// 
+/// Lifetime of the returned slice is the same as the input string
+fn find_in_string<'a>(s: &'a str, sub: &str) -> Option<&'a str> {
+    let match_byte = s.find(sub);
+    match match_byte {
+        Some(x) => Some(&s[x..x+sub.len()]),
+        None => None
+    }
 }
 
 // DO NOT MODIFY BELOW THIS LINE
